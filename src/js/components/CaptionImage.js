4f3511adfc5746128de5cee1074b0a4d630319bd
JordynLewi5 <jlewis162030@gmail.com>
2024-01-12 14:53:26 -0600

    new commit

--- src/js/components/CaptionImage.js
+++ src/js/components/CaptionImage.js
@@ -1,13 +1,13 @@
 function CaptionImage(props) {
     return (
-        <a className={`caption-image ${props.className}`} href={props.href} target="__">
-            <div className="overlay">
+        <div className={`caption-image ${props.className}`} target="__">
+            <a className="overlay" href={props.href}>
                 <img className="image" src={props.src} alt={props.alt}/>
                 <div className="caption">
                     {props.caption}
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     )
 }
 
--- src/js/pages/Projects.js
+++ src/js/pages/Projects.js
@@ -8,10 +8,10 @@ function Projects() {
             <TypeWriter text="<Projects 🚀 />" cursor_blink={true} delay={0} offset={-900} duration={50} className="projects-header"/>
             
             <div className="project-card-list">
-                <ProjectCard name="Empire State Building Lights" offset={-850} href="/esblights" target="" src="/images/esblights-thumbnail.png" alt="Empire State Building Lights" />
-                <ProjectCard name="Empire State Building Lights Hue" offset={-800} href="/esblights-hue" target="" src="/images/weathervane.png" alt="Empire State Building Lights Hue System" />
-                <ProjectCard name="Partie Discord Bot" offset={-750} href="/partie" target="" src="/images/partie-cropped.gif" alt="Partie Discord Bot" />
-                <ProjectCard name="Rowing Guide" offset={-700} href="/rowing-guide" target="" src="/images/rowingguide.png" alt="Rowing Guide" />
+                <ProjectCard name="Empire State Building Lights" offset={-600} href="/esblights" target="" src="/images/esblights-thumbnail.png" alt="Empire State Building Lights" />
+                <ProjectCard name="Empire State Building Lights Hue" offset={-650} href="/esblights-hue" target="" src="/images/weathervane.png" alt="Empire State Building Lights Hue System" />
+                <ProjectCard name="Partie Discord Bot" offset={-700} href="/partie" target="" src="/images/partie-cropped.gif" alt="Partie Discord Bot" />
+                <ProjectCard name="Rowing Guide" offset={-600} href="/rowing-guide" target="" src="/images/rowingguide.png" alt="Rowing Guide" />
 
             </div>
         </div>
